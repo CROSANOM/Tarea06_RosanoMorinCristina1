109fece0b82c072db961b522fe631f3964a5fd87
crosanom <crosanom@192.168.1.40>
2018-02-19 22:56:52 +0100

    Commit 9 Crea también la clase Consola dentro de dicho paquete y utiliza los métodos de ayuda de dicha clase en la clase anterior. Comprueba que todo sigue funcionando correctamente.

--- src/alquilerVehiculos/mvc/vista/IUTextual.java
+++ src/alquilerVehiculos/mvc/vista/IUTextual.java
@@ -14,81 +14,21 @@ public class IUTextual {
 		 * @param args
 		 */
 		public static void main(String[] args) {
+		
+			// Cuando haya interfaz habrá que tener un atributo del tipo de interfaz.
+			// IControladorAlquilerVehiculos controlador;
+			
+			// Esta clase dejara de ser la main.
+			// Esta clase representa la vista de la aplicacion.
+			// Cuando tengas interface, sera un objeto del interface. Mientras será la clase directamente.
+			AlquilerVehiculos alquilerVehiculosFuturoControlador = new AlquilerVehiculos();
 	
-			// instanciar la clase AlquilerVehiculos
-			AlquilerVehiculos miAlquiler = new AlquilerVehiculos();
-	//		System.out.println("Ejemplo de nuevo cliente");
-	//		// instancia de clientes 
-	//		Cliente clienteP=new Cliente ("Maria","75675656R",new DireccionPostal("calleA","Tarif","11380"));
-	//		System.out.println("Maria " + clienteP.toString());
-	//		
-	//		Cliente clienteP2=new Cliente ("Manolo","75675657R",new DireccionPostal("calleB","Tarif","11380"));
-	//		System.out.println("Maria " + clienteP2.toString());
-	//
-	//		DireccionPostal nuevaDirPostal = new DireccionPostal("La viña", "cadiz", "04007");
-	//		System.out.println("Ejemplo de una direccionPostal " + nuevaDirPostal.toString());
-	//	
-	//	
-	//	// instancia Turismos
-	//	
-	//			Turismo turismoP = new Turismo("2223GTY", "Seat", "Panda", 1300);
-	//			Turismo turismoP2 = new Turismo("1111YTG", "Citroen", "Picaso", 200);
-	//			Turismo turismoP3 = new Turismo("2111YTG", "Citroen", "Cactus", 1200);
-	//
-	//			
-	//			System.out.println(" Ejemplo de turismo" + turismoP2.toString()+turismoP.toString());
-	//			System.out.println(" Ejemplo de turismo3 y turismo" + turismoP3.toString()+turismoP.toString());
-	//			System.out.println("Disponibilidad");
-	//			System.out.println("\n---------------");
-	//			System.out.println("turismoP3"+turismoP3.getDisponible());
-	//
-	//			
-	//	// instanciar alquiler  probrar sus metodos 
-	//			
-	//			
-	//			Alquiler alquierp =new Alquiler(clienteP,turismoP2);
-	//			System.out.println(" Ejemplo de Alquiler" + alquierp.toString()+turismoP2.toString());
-	//			System.out.println("Situación del vehiculo p2 esta disponible"+ turismoP2.getDisponible());
-	//
-	//		// abriralquiler
-	//			miAlquiler.anadirTurismo(turismoP2);
-	//			miAlquiler.abrirAlquiler(clienteP, turismoP);
-	//			System.out.println("Ver mi alquiler"+ miAlquiler.toString()+ "ver disponibilidad de turismoP" + turismoP.getDisponible());
-	//			  
-	//		    //miAlquiler.abrirAlquiler(clienteP, turismoP);salta la excepcion y como no hay try Catch para programa
-	//		
-	//			Turismo turismoP4 = new Turismo("2111YYG", "Citroen", "Cactus", 1200);
-	//			System.out.println("Ver mi alquiler"+ miAlquiler.toString()+ "ver disponibilidad de turismoP" + turismoP4.getDisponible());
-	//			// borrarTurismo
-	//			
-	////		miAlquiler.borrarTurismo("2111YYG");
-	////		System.out.println("este es mi alquiler" + miAlquiler.toString());
-	//			
-	//			// cerraralquiler 
-	//			
-	//			miAlquiler.cerrarAlquiler(turismoP);
-	//			System.out.println("Ver mi alquiler"+ miAlquiler.toString());
-	//			System.out.println("Disponibilidad de "+turismoP.getDisponible() + "disponibilidad de " +turismoP2.getCilindrada()+turismoP2.getDisponible());
-	//			
-	//		
-	//	
-	//	// for para recorrer y recuperar cliente, turismo y alquiler print cliente ,
-	//			// turismos, alquileres
-	//
-	//			for (Cliente cliente : miAlquiler.ObtenerCliente()) // clase AlquilerVehiculos
-	//				if (cliente != null)
-	//					System.out.println(cliente);
-	//			System.out.println("--------------");
-	//			for (Turismo turismo : miAlquiler.ObtenerTurismos())// clase AlquilerVehiculos
-	//				if (turismo != null)
-	//					System.out.println(turismo);
-	//			System.out.println("--------------");
-	//			for (Alquiler alquiler : miAlquiler.obtenerAlquileres())
-	//				if (alquiler != null)
-	//					System.out.println(alquiler);
-	//
-	//			// do - while valida la entrada menu
-	
+			// Esto será un futuro metodo del interfaz para setear el controlador.
+			AlquilerVehiculos controlador = alquilerVehiculosFuturoControlador;
+			
+			
+			// Se utilizara el metodo del interfaz para lanzar el menu.
+			//
 				int opcion;
 				do {
 	
@@ -112,166 +52,42 @@ public class IUTextual {
 						opcion = Entrada.entero();
 	
 					} while (opcion < 0 || opcion > 9); // entra en bucle
+					
+					// OPCIONES DE MENU.
 					switch (opcion) {
 	
 					case 1: // Añadir cliente
-						Cliente nuevoCliente = null;
-						do {
-	
-							System.out.println("\n añadir DireccionPostal");
-							System.out.println("--------------");
-							System.out.print("Calle");
-							String callet1 = Entrada.cadena();
-							System.out.print("Localidad: ");
-							String localidadt1 = Entrada.cadena();
-							System.out.print("codigoPostal: ");
-							String codigoPostalt1 = Entrada.cadena();
-	
-							// instanciar un objeto direccionPostal con parametros leidos por teclado
-							DireccionPostal direccionPoT = new DireccionPostal(callet1, localidadt1,
-									codigoPostalt1);
-	
-							System.out.println("\n añadir cliente");
-							System.out.println("--------------");
-							System.out.print("Nombre: ");
-							String nombreT = Entrada.cadena();
-							System.out.print("DNI: ");
-							String dniT = Entrada.cadena();
-	
-							// prueba añadir cliente
-							try {
-								nuevoCliente = new Cliente(nombreT, dniT, direccionPoT);
-							} catch (ExcepcionAlquilerVehiculos e) {// si error
-								System.out.printf("ERROR: %s%n%n", e.getMessage());
-								System.out.println("Vuelve a introducir los datos de forma correcta");
-							}
-						} while (nuevoCliente == null);
-						try {
-							miAlquiler.anadirCliente(nuevoCliente);
-						} catch (ExcepcionAlquilerVehiculos e) {
-							System.out.printf("ERROR: %s%n%n", e.getMessage());
-						}
-	
-						System.out.println("Cliente " + nuevoCliente.toString());
+						anadirCliente(alquilerVehiculosFuturoControlador);
 						break;
 					case 2: // borrar Cliente
-						System.out.println("\nBorrar cliente");
-						System.out.println("--------------");
-						System.out.print("\nIntroduce el DNI del cliente a borrar: ");
-						String dniBorrar = Entrada.cadena();
-						try {
-							miAlquiler.borrarCliente(dniBorrar);
-							System.out.println("Cliente borrado satisfactoriamente\n");
-						} catch (Exception e) {
-							System.out.printf("ERROR: %s%n%n", e.getMessage());
-						}
+						borrarCliente(alquilerVehiculosFuturoControlador);
 						break;
 	
 					case 3: // listar Cliente
-						System.out.println("\nListado de clientes");
-						System.out.println("-------------------");
-						for (Cliente cliente : miAlquiler.ObtenerCliente()) {
-							if (cliente != null)
-								System.out.println(cliente.toString());
-						}
-						System.out.println("");
+						listarCliente(alquilerVehiculosFuturoControlador);
 						break;
 					case 4: // añadir turismo
-						Turismo nuevoTurismo = null;
-						System.out.println("\n añadir Turismo");
-						System.out.println("---------------");
-						System.out.println("Matricula: ");
-						String matricula = Entrada.cadena();
-						System.out.print("Marca: ");
-						String marca = Entrada.cadena();
-						System.out.print("Modelo: ");
-						String modelo = Entrada.cadena();
-						System.out.print("Cilindrada: ");
-						int cilindrada = Entrada.entero();
-						try {
-							nuevoTurismo = new Turismo(matricula, marca, modelo, cilindrada);
-							miAlquiler.anadirTurismo(nuevoTurismo);
-						} catch (ExcepcionAlquilerVehiculos e) {
-							System.out.printf("ERROR: %s%n%n", e.getMessage());
-						}
-						System.out.println("Turismo " + nuevoTurismo.toString());
+						anadirTurismo(alquilerVehiculosFuturoControlador);
 						break;
 					case 5: // borrar turismo
-						System.out.println("\nBorrar Turismo");
-						System.out.println("---------------");
-						System.out.print("\nIntroduce la matriÂ­cula del vehiculo a borrar: ");
-						String matriculaBorrar = Entrada.cadena();
-						try {
-							miAlquiler.borrarTurismo(matriculaBorrar);
-							System.out.println("Turismo borrado satisfactoriamente\n");
-						} catch (ExcepcionAlquilerVehiculos e) {
-							System.out.printf("ERROR: %s%n%n", e.getMessage());
-						}
+						borrarTurismo(alquilerVehiculosFuturoControlador);
 						break;
 	
 					case 6: // listar turismo
-						System.out.println("\nListado de Turismo");
-						System.out.println("--------------------");
-						for (Turismo turismo : miAlquiler.ObtenerTurismos()) {
-							if (turismo != null)
-								System.out.println(turismo);
-	
-						}
-						System.out.println("");
+						listarTurismo(alquilerVehiculosFuturoControlador);
 						break;
 	
 					case 7:// abrir alquiler
-						System.out.println("\nAbrir Alquiler");
-						System.out.println("-------------");
-						System.out.print("\nIntroduce la matriÂ­cula del vehiÂ­culo: ");
-						String matriculaBuscar = Entrada.cadena();
-						Turismo turismoBuscado = miAlquiler.buscarTurismo(matriculaBuscar);
-						System.out.println("\nIntroduce el dni");
-						String dniT2 = Entrada.cadena();
-						Cliente cliente = miAlquiler.buscarCliente(dniT2);
-	
-						if (turismoBuscado == null)
-							System.out.println("ERROR: No existe un vehiÂ­culo con dicha matricula\n");
-						else {
-							try {
-								miAlquiler.abrirAlquiler(cliente, turismoBuscado);
-								System.out.println("Alquiler abierto satisfactoriamente\n");
-	
-							} catch (ExcepcionAlquilerVehiculos e) {
-								System.out.printf("ERROR: %s%n%n", e.getMessage());
-							}
-						}
+						abrirAlquiler(alquilerVehiculosFuturoControlador);
 	
 						break;
 	
 					case 8:
-						System.out.println("\n Cerrar Alquiler");
-						System.out.println("--------------");
-						System.out.print("\nIntroduce la matricula del vehiÂ­culo: ");
-						matriculaBuscar = Entrada.cadena();
-						turismoBuscado = miAlquiler.buscarTurismo(matriculaBuscar);
-	
-						if (turismoBuscado == null)
-							System.out.println("ERROR: No existe un vehiculo con dicha matriÂ­cula\n");
-						else {
-							try {
-								miAlquiler.cerrarAlquiler(turismoBuscado);
-								System.out.println("Alquiler cerrado satisfactoriamente");
-								System.out.println("Alquiler cerrado =" + miAlquiler.getAlquileres());
-							} catch (ExcepcionAlquilerVehiculos e) {
-								System.out.printf("ERROR: %s%n%n", e.getMessage());
-							}
-						}
+						cerrarAlquiler(alquilerVehiculosFuturoControlador);
 						break;
-	
+						
 					case 9: // listado alquiler
-						System.out.println("\nListado de Alquileres");
-						System.out.println("---------------------");
-						for (Alquiler alquiler : miAlquiler.obtenerAlquileres()) {
-							if (alquiler != null)
-								System.out.println("Estos son los alquileres:\n" + alquiler + "ver los alquileres"
-										+ miAlquiler.toString());
-						}
+						listarAlquileres(alquilerVehiculosFuturoControlador);
 	
 						break;
 					default:
@@ -282,4 +98,216 @@ public class IUTextual {
 	
 		}
 
+	/**
+	 * @param miAlquiler
+	 */
+	public static void listarAlquileres(AlquilerVehiculos miAlquiler) {
+		System.out.println("\nListado de Alquileres");
+		System.out.println("---------------------");
+		for (Alquiler alquiler : miAlquiler.obtenerAlquileres()) {
+			if (alquiler != null)
+				System.out.println("Estos son los alquileres:\n" + alquiler + "ver los alquileres"
+						+ miAlquiler.toString());
+		}
+	}
+
+	/**
+	 * @param miAlquiler
+	 */
+	public static void cerrarAlquiler(AlquilerVehiculos miAlquiler) {
+		System.out.println("\n Cerrar Alquiler");
+		System.out.println("--------------");
+		System.out.print("\nIntroduce la matricula del vehiÂ­culo: ");
+		String matriculaBuscar = Entrada.cadena();
+		Turismo turismoBuscado = miAlquiler.buscarTurismo(matriculaBuscar);
+
+		if (turismoBuscado == null)
+			System.out.println("ERROR: No existe un vehiculo con dicha matriÂ­cula\n");
+		else {
+			try {
+				miAlquiler.cerrarAlquiler(turismoBuscado);
+				System.out.println("Alquiler cerrado satisfactoriamente");
+				System.out.println("Alquiler cerrado =" + miAlquiler.getAlquileres());
+			} catch (ExcepcionAlquilerVehiculos e) {
+				System.out.printf("ERROR: %s%n%n", e.getMessage());
+			}
+		}
+	}
+
+	/**
+	 * @param miAlquiler
+	 */
+	public static void abrirAlquiler(AlquilerVehiculos miAlquiler) {
+		System.out.println("\nAbrir Alquiler");
+		System.out.println("-------------");
+		System.out.print("\nIntroduce la matriÂ­cula del vehiÂ­culo: ");
+		String matriculaBuscar = Entrada.cadena();
+		Turismo turismoBuscado = miAlquiler.buscarTurismo(matriculaBuscar);
+		System.out.println("\nIntroduce el dni");
+		String dniT2 = Entrada.cadena();
+		Cliente cliente = miAlquiler.buscarCliente(dniT2);
+
+		if (turismoBuscado == null)
+			System.out.println("ERROR: No existe un vehiÂ­culo con dicha matricula\n");
+		else {
+			try {
+				miAlquiler.abrirAlquiler(cliente, turismoBuscado);
+				System.out.println("Alquiler abierto satisfactoriamente\n");
+
+			} catch (ExcepcionAlquilerVehiculos e) {
+				System.out.printf("ERROR: %s%n%n", e.getMessage());
+			}
+		}
+	}
+
+	/**
+	 * @param miAlquiler
+	 */
+	public static void listarTurismo(AlquilerVehiculos miAlquiler) {
+		System.out.println("\nListado de Turismo");
+		System.out.println("--------------------");
+		for (Turismo turismo : miAlquiler.ObtenerTurismos()) {
+			if (turismo != null)
+				System.out.println(turismo);
+
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * @param miAlquiler
+	 */
+	public static void borrarTurismo(AlquilerVehiculos miAlquiler) {
+		System.out.println("\nBorrar Turismo");
+		System.out.println("---------------");
+		System.out.print("\nIntroduce la matriÂ­cula del vehiculo a borrar: ");
+		String matriculaBorrar = Entrada.cadena();
+		try {
+			miAlquiler.borrarTurismo(matriculaBorrar);
+			System.out.println("Turismo borrado satisfactoriamente\n");
+		} catch (ExcepcionAlquilerVehiculos e) {
+			System.out.printf("ERROR: %s%n%n", e.getMessage());
+		}
+	}
+
+	/**
+	 * @param miAlquiler
+	 */
+	public static void borrarCliente(AlquilerVehiculos miAlquiler) {
+		System.out.println("\nBorrar cliente");
+		System.out.println("--------------");
+		System.out.print("\nIntroduce el DNI del cliente a borrar: ");
+		String dniBorrar = Entrada.cadena();
+		try {
+			miAlquiler.borrarCliente(dniBorrar);
+			System.out.println("Cliente borrado satisfactoriamente\n");
+		} catch (Exception e) {
+			System.out.printf("ERROR: %s%n%n", e.getMessage());
+		}
+	}
+
+	/**
+	 * @param miAlquiler
+	 */
+	public static void anadirCliente(AlquilerVehiculos miAlquiler) {
+		Cliente nuevoCliente = null;
+		do {
+
+			System.out.println("\n añadir DireccionPostal");
+			System.out.println("--------------");
+			System.out.print("Calle");
+			String callet1 = Entrada.cadena();
+			System.out.print("Localidad: ");
+			String localidadt1 = Entrada.cadena();
+			System.out.print("codigoPostal: ");
+			String codigoPostalt1 = Entrada.cadena();
+
+			// instanciar un objeto direccionPostal con parametros leidos por teclado
+			DireccionPostal direccionPoT = new DireccionPostal(callet1, localidadt1,
+					codigoPostalt1);
+
+			System.out.println("\n añadir cliente");
+			System.out.println("--------------");
+			System.out.print("Nombre: ");
+			String nombreT = Entrada.cadena();
+			System.out.print("DNI: ");
+			String dniT = Entrada.cadena();
+
+			// prueba añadir cliente
+			try {
+				nuevoCliente = new Cliente(nombreT, dniT, direccionPoT);
+			} catch (ExcepcionAlquilerVehiculos e) {// si error
+				System.out.printf("ERROR: %s%n%n", e.getMessage());
+				System.out.println("Vuelve a introducir los datos de forma correcta");
+			}
+		} while (nuevoCliente == null);
+		try {
+			miAlquiler.anadirCliente(nuevoCliente);
+		} catch (ExcepcionAlquilerVehiculos e) {
+			System.out.printf("ERROR: %s%n%n", e.getMessage());
+		}
+
+		System.out.println("Cliente " + nuevoCliente.toString());
+	}
+
+	/**
+	 * @param miAlquiler
+	 */
+	public static void anadirTurismo(AlquilerVehiculos miAlquiler) {
+		Turismo nuevoTurismo = null;
+		System.out.println("\n añadir Turismo");
+		System.out.println("---------------");
+		System.out.println("Matricula: ");
+		String matricula = Entrada.cadena();
+		System.out.print("Marca: ");
+		String marca = Entrada.cadena();
+		System.out.print("Modelo: ");
+		String modelo = Entrada.cadena();
+		System.out.print("Cilindrada: ");
+		int cilindrada = Entrada.entero();
+		try {
+			nuevoTurismo = new Turismo(matricula, marca, modelo, cilindrada);
+			miAlquiler.anadirTurismo(nuevoTurismo);
+		} catch (ExcepcionAlquilerVehiculos e) {
+			System.out.printf("ERROR: %s%n%n", e.getMessage());
+		}
+		System.out.println("Turismo " + nuevoTurismo.toString());
+	}
+
+	/**
+	 * @param miAlquiler
+	 */
+	public static void listarCliente(AlquilerVehiculos miAlquiler) {
+		System.out.println("\nListado de clientes");
+		System.out.println("-------------------");
+		for (Cliente cliente : miAlquiler.ObtenerCliente()) {
+			if (cliente != null)
+				System.out.println(cliente.toString());
+		}
+		System.out.println("");
+	}
+
+	
+	// Borrar luego
+	public void salir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void borrar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void buscar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void listarTurismo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
--- src/alquilerVehiculos/mvc/vista/utilidades/Consola.java
+++ src/alquilerVehiculos/mvc/vista/utilidades/Consola.java
@@ -1,5 +1,108 @@
-package mvc.vista.utilidades;
+package alquilerVehiculos.mvc.vista.utilidades;
 
+import alquilerVehiculos.mvc.modelo.dominio.Cliente;
+import alquilerVehiculos.mvc.modelo.dominio.DireccionPostal;
+import alquilerVehiculos.mvc.modelo.dominio.Turismo;
+import alquilerVehiculos.mvc.vista.Opcion;
+
+// Esta clase contiene toda la entrada y salida de datos de la aplicacion
+
+// metodos staticos: mostrarMenu, MostrarCabecera,  leer cliente, turismos, matricula, Dni, 
 public class Consola {
+	
+	// constructor 
+	public Consola() {
+		
+	}
+	
+// mostrar Menu 
+	public static void mostrarMenu() {
+		mostrarCabecera("Alquiler");
+		for (Opcion opcion: Opcion.values()) {
+			System.out.println(opcion);
+		}
+	}
+	
+	// Cabeceras de los listados de las opciones.
+	public static void mostrarCabecera(String mensaje) {
+		System.out.printf("%n%s%n", mensaje);
+		System.out.println(String.format("%0" + mensaje.length() + "d%n", 0).replace("0", "-"));
+	}
+	
 
+//	// Elegir Opción 
+//	public static int elegirOpcion() {
+//		int ordinalOpcion;
+//		do {
+//			System.out.print("\nElige una opcion: ");
+//			ordinalOpcion = Entrada.entero();
+//		} while (!Opcion.esOrdinalValido(ordinalOpcion));
+//		return ordinalOpcion;
+//	}
+	// 	leerCliente 
+	public static Cliente leerCliente() {
+		Cliente cliente = null;
+		System.out.print("Nombre: ");
+		String nombre = Entrada.cadena();
+		System.out.print("DNI: ");
+		String dni = Entrada.cadena();
+		System.out.print("Direccion: ");
+		String direccion = Entrada.cadena();
+		System.out.print("Localidad: ");
+		String localidad = Entrada.cadena();
+		System.out.print("CodigoPostal: ");
+		String codigoPostal = Entrada.cadena();
+		cliente = new Cliente(nombre, dni,  new DireccionPostal(direccion, localidad, codigoPostal));
+		return cliente;
+	}
+	// leerDni
+	public static String leerDni() {
+		System.out.print("Introduce el DNI del cliente: ");
+		String dniBorrar = Entrada.cadena();
+		return dniBorrar;
+	}
+	// 
+	public static Turismo leerTurismo(Cliente cliente) {
+		Turismo nuevoTurismo = null;
+		System.out.print("Matrcula: ");
+		String matricula = Entrada.cadena();
+		System.out.print("Marca: ");
+		String marca = Entrada.cadena();
+		System.out.print("Modelo: ");
+		String modelo = Entrada.cadena();
+		System.out.print("Cilindrada: ");
+		int cilindrada = Entrada.entero();
+		nuevoTurismo = new Turismo( matricula, marca, modelo,  cilindrada);
+		return nuevoTurismo;
+	}
+	
+	public static String leerMatricula() {
+		System.out.print("Introduce la matricula del turismo: ");
+		String matriculaBorrar = Entrada.cadena();
+		return matriculaBorrar;
+	}
+	
+
+
+//	public static int elegirTipoVehiculo() {
+//		int ordinalTipoVehiculo;
+//		do {
+//			System.out.printf("Elige el tipo de trabajo: ( %s)", obtenerTiposTrabajo());
+//			ordinalTipoTrabajo = Entrada.entero();
+//		} while (!TipoTrabajo.esOrdinalValido(ordinalTipoTrabajo));
+//		return ordinalTipoTrabajo;
+//	}
+//	
+//	private static String obtenerTiposTrabajo() {
+//		StringBuilder tiposTrabajos = new StringBuilder("");
+//		for (TipoTrabajo tipoTrabajo: TipoTrabajo.values()) {
+//			tiposTrabajos.append(tipoTrabajo.ordinal()).append(".- ").append(tipoTrabajo).append(" ");
+//		}
+//		return tiposTrabajos.toString();
+//	}
+//
+//}
+
+	
+	
 }
