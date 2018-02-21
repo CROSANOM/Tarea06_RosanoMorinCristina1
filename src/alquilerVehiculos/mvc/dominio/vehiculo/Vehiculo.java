e5b8102d2ded111a704203c82ef3b05dd3ac53ef
crosanom <crosanom@192.168.1.40>
2018-02-21 21:39:27 +0100

    5º commit segunda parte: Crear clase Turismo que hereda de Vehiculo.

--- src/alquilerVehiculos/mvc/dominio/vehiculo/Vehiculo.java
+++ src/alquilerVehiculos/mvc/dominio/vehiculo/Vehiculo.java
@@ -1,30 +1,32 @@
 package alquilerVehiculos.mvc.dominio.vehiculo;
+
 import java.util.regex.*;
 
 import alquilerVehiculos.mvc.modelo.dominio.ExcepcionAlquilerVehiculos;
+import alquilerVehiculos.mvc.modelo.dominio.vehiculo.TipoVehiculo;
+
 
 /**
  * @author crosanom version2
  *
  */
 
-public class Vehiculo {
+public  class Vehiculo {
 
-	
 	private String matricula, marca, modelo;
 	private boolean disponible = true; // un coche disponible
 
 	// anadir los atributos de Vehiculo
-	
-	private final double  FACTOR_CILINDRADA=1200;
-	private final double FACTOR_NUMERO_PLAZAS_=1;
-	private final double FACTOR_PMA =10;
-	private DatosTecnicosVehiculo datosTecnicos;
-	
-	//Constructor ()
-	
-	public Vehiculo(String matricula, String marca, String modelo,  DatosTecnicosVehiculo datosTecnicos) {
-		
+
+	protected final double FACTOR_CILINDRADA = 1200;
+	protected final double FACTOR_NUMERO_PLAZAS = 1;
+	protected final double FACTOR_PMA = 10;
+	protected DatosTecnicosVehiculo datosTecnicos;
+
+	// Constructor ()
+
+	public Vehiculo(String matricula, String marca, String modelo, DatosTecnicosVehiculo datosTecnicos) {
+
 		setMatricula(matricula);
 		setMarca(marca);
 		setModelo(modelo);
@@ -32,20 +34,25 @@ public class Vehiculo {
 
 	}
 
-	
-	// constructor copia 
-	
+	// constructor copia
+
 	public Vehiculo(Vehiculo vehiculo) {
 
-	matricula=vehiculo.getMatricula();
-	marca=vehiculo.getMarca();
-	modelo=vehiculo.getModelo();
-	datosTecnicos=vehiculo.getDatosTecnicos();
-	
+		matricula = vehiculo.getMatricula();
+		marca = vehiculo.getMarca();
+		modelo = vehiculo.getModelo();
+		datosTecnicos = vehiculo.getDatosTecnicos();
+
 	}
-	
-	// get datos tecnicos 
-	
+
+//	// metodos abstract
+//
+//	public abstract double getPrecioEspecifico();
+//
+//	public abstract TipoVehiculo getTipoVehiculo();
+
+	// get datos tecnicos
+
 	/**
 	 * @return the fACTOR_CILINDRADA
 	 */
@@ -53,14 +60,12 @@ public class Vehiculo {
 		return FACTOR_CILINDRADA;
 	}
 
-
 	/**
 	 * @return the fACTOR_NUMERO_PLAZAS_
 	 */
 	public double getFACTOR_NUMERO_PLAZAS_() {
-		return FACTOR_NUMERO_PLAZAS_;
+		return FACTOR_NUMERO_PLAZAS;
 	}
-
 
 	/**
 	 * @return the fACTOR_PMA
@@ -76,63 +81,63 @@ public class Vehiculo {
 		return datosTecnicos;
 	}
 
-
 	private void setDatosTecnicosVehiculo(DatosTecnicosVehiculo datosTecnicos) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
 	public void setDatosTecnicosVehiculos(DatosTecnicosVehiculo datosTecnicosVehiculos) {
-	 
+
 	}
-	
-		/**
-		 * @param disponible
-		 *            the disponibilidad to set
-		 */
-		public void setDisponible(boolean disponible) {
-			this.disponible = disponible;
-		}
 
-		/**
-		 * @param matricula the matricula to set
-		 */
-		private void setMatricula(String matricula) {
-			
-			// Comprobar Matricula
-			boolean matriculaCorrecta = false;
-			matriculaCorrecta = compruebaMatricula(matricula);
+	/**
+	 * @param disponible
+	 *            the disponibilidad to set
+	 */
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
 
-			if (matriculaCorrecta) {
-				this.matricula = matricula; // inicializar matricula
-			} else
-				throw new ExcepcionAlquilerVehiculos("La matricula No es correcta"); // lanzar excepción
-		}
+	/**
+	 * @param matricula
+	 *            the matricula to set
+	 */
+	private void setMatricula(String matricula) {
 
-		/**
-		 * @param marca the marca to set
-		 */
-		private void setMarca(String marca) {
-			
-			//if (marca!=null && marca.equals(""))
-				this.marca = marca;
-			//else 
-				//throw new ExcepcionAlquilerVehiculos("La marca no es valida");
-		}
+		// Comprobar Matricula
+		boolean matriculaCorrecta = false;
+		matriculaCorrecta = compruebaMatricula(matricula);
 
-		/**
-		 * @param modelo the modelo to set
-		 */
-		private void setModelo(String modelo) {
-			
-			//if (modelo!=null && modelo.equals(""))
-				this.modelo = modelo;
-			//else 
-				//throw new ExcepcionAlquilerVehiculos("El modelo no es valido");
-		}
+		if (matriculaCorrecta) {
+			this.matricula = matricula; // inicializar matricula
+		} else
+			throw new ExcepcionAlquilerVehiculos("La matricula No es correcta"); // lanzar excepción
+	}
 
-	
+	/**
+	 * @param marca
+	 *            the marca to set
+	 */
+	private void setMarca(String marca) {
+
+		// if (marca!=null && marca.equals(""))
+		this.marca = marca;
+		// else
+		// throw new ExcepcionAlquilerVehiculos("La marca no es valida");
+	}
+
+	/**
+	 * @param modelo
+	 *            the modelo to set
+	 */
+	private void setModelo(String modelo) {
+
+		// if (modelo!=null && modelo.equals(""))
+		this.modelo = modelo;
+		// else
+		// throw new ExcepcionAlquilerVehiculos("El modelo no es valido");
+	}
+
 	// Metodo para comprobar matricula usando expresión regular
 	private boolean compruebaMatricula(String matricula) {
 
@@ -155,8 +160,7 @@ public class Vehiculo {
 
 	}
 
-	
-	// metodos getters 
+	// metodos getters
 	/**
 	 * @return the matricula
 	 */
@@ -178,7 +182,6 @@ public class Vehiculo {
 		return modelo;
 	}
 
-
 	/**
 	 * @return the disponibilidad
 	 */
@@ -186,23 +189,18 @@ public class Vehiculo {
 		return disponible;
 	}
 
-	
-	
 	// toString actualizado con datosTecnicos
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
 		return "Vehiculo [matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", disponible="
 				+ disponible + ", FACTOR_CILINDRADA=" + FACTOR_CILINDRADA + ", FACTOR_NUMERO_PLAZAS_="
-				+ FACTOR_NUMERO_PLAZAS_ + ", FACTOR_PMA=" + FACTOR_PMA + ", datosTecnicos=" + datosTecnicos + "]";
+				+ FACTOR_NUMERO_PLAZAS + ", FACTOR_PMA=" + FACTOR_PMA + ", datosTecnicos=" + datosTecnicos + "]";
 	}
 
-	
-	
-
-	
-	
 }
--- src/alquilerVehiculos/mvc/modelo/dominio/vehiculo/Turismo.java
+++ src/alquilerVehiculos/mvc/modelo/dominio/vehiculo/Turismo.java
@@ -1,9 +1,48 @@
-package alquilerVehiculos.mvc.dominio.vehiculo;
+package alquilerVehiculos.mvc.modelo.dominio.vehiculo;
 
-public class Turismo {
+import alquilerVehiculos.mvc.dominio.vehiculo.DatosTecnicosVehiculo;
+import alquilerVehiculos.mvc.dominio.vehiculo.Vehiculo;
 
-	public Turismo(Vehiculo vehiculo) {
-		// TODO Auto-generated constructor stub
+public class Turismo extends Vehiculo {
+
+	// atributo Turismo
+	double precioEspecificoTurismo = 0;
+
+	// constructor:
+	// public Vehiculo(String matricula, String marca, String modelo,
+	// DatosTecnicosVehiculo datosTecnicos) {
+
+	/**
+	 * @param matricula
+	 * @param marca
+	 * @param modelo
+	 * @param datosTecnicos
+	 */
+	public Turismo(String matricula, String marca, String modelo, DatosTecnicosVehiculo datosTecnicos) {
+		super(matricula, marca, modelo, datosTecnicos);
+
 	}
 
+	// constructor Copia
+
+	/**
+	 * @param vehiculo
+	 */
+	public Turismo(Vehiculo vehiculo) {
+		super(vehiculo);
+	}
+
+	public TipoVehiculo getTipoVehiculo() {
+
+		return TipoVehiculo.TURISMO;
+	}
+
+	public double getPrecioEspecifico() {
+
+		// calcular precio especifico Turismo
+
+		precioEspecificoTurismo = (getPrecioEspecifico() * FACTOR_CILINDRADA / 50);
+		return precioEspecificoTurismo;
+	}
+	
 }
